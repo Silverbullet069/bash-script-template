684dfad4ec9b16e4bf1cc7e9a52b8e6d14104060
Silverbullet069 <hackiemsi2002@gmail.com>
2025-02-14 21:03:23 +0700

    feat: add `--save-log` option and script header

--- script.sh
+++ script.sh
@@ -1,11 +1,97 @@
 #!/usr/bin/env bash
 
-# A best practices Bash script template with many useful functions. This file
-# sources in the bulk of the functions from the source.sh file which it expects
-# to be in the same directory. Only those functions which are likely to need
-# modification are present in this file. This is a great combination if you're
-# writing several scripts! By pulling in the common functions you'll minimise
-# code duplication, as well as ease any potential updates to shared functions.
+## FILE        : #~NAME~#
+## DESCRIPTION : General Bash script template
+## AUTHOR      : Silverbullet069
+## CREATED     : #~TIME~#
+## VERSION     : 0.1
+## LICENSE     : MIT License
+## CREDIT      : https://github.com/ralish/bash-script-template/blob/main/script.sh
+
+## ToC:
+## - usage()
+## - simple_parse_options()
+## - main()
+
+# ============================================================================ #
+
+# DESC: Usage help
+# ARGS: None
+# OUTS: None
+# RETS: None
+function usage() {
+    cat << EOF
+
+#~NAME~# [-c] [-nc] [-nt]
+
+Usage:
+    -h|--help                 Displays this help
+    -q|--quiet                Run silently unless we encounter an error
+    -c|--no-color             Disables color output
+    -t|--no-timestamp         Disables timestamp output
+    -s|--save-log             Save terminal output to log file
+EOF
+}
+
+# DESC: Parameter parser
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: Variables indicating command-line parameters and options
+# RETS: None
+function simple_parse_options() {
+
+    while [[ $# -gt 0 ]]; do
+        local _param="$1"
+        shift
+        case $_param in
+            -h | --help)
+                usage
+                exit 0
+                ;;
+            -q | --quiet)
+                readonly QUIET=1
+                ;;
+            -c | --no-color)
+                readonly NO_COLOR=1
+                ;;
+            -t | --no-timestamp)
+                readonly NO_TIMESTAMP=1
+                ;;
+            -s | --save-log)
+                readonly LOG_FILE="${SCRIPT_DIR}/logs/$(basename "${0%.*}").log"
+                if [[ ! -d "$(dirname "$LOG_FILE")" ]]; then
+                  local -r _mkdir_log=$(mktemp)
+                  mkdir -pv "$(dirname "$LOG_FILE")" | tee "$_mkdir_log"
+                  cat "$_mkdir_log" >> "$LOG_FILE"
+                  rm "$_mkdir_log"
+                fi
+                ;;
+            *)
+                exit_script "Invalid parameter was provided: $_param" 1
+                ;;
+        esac
+    done
+}
+
+# DESC: Main control flow
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: None
+# RETS: None
+function main() {
+    trap trap_script_err ERR
+    trap trap_script_exit EXIT
+
+    init_script "$@"
+    simple_parse_options "$@"
+    init_color
+    init_quiet
+    init_lock user
+
+    # Start Here
+}
+
+# ======================================================== #
+# Initialize Safety Flags And Run The Main Script          #
+# ======================================================== #
 
 # Enable xtrace if the DEBUG environment variable is set
 if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
@@ -24,67 +110,14 @@ fi
 # Enable errtrace or the error trap handler will not work as expected
 set -o errtrace         # Ensure the error trap handler is inherited
 
-# DESC: Usage help
-# ARGS: None
-# OUTS: None
-# RETS: None
-function script_usage() {
-    cat << EOF
-Usage:
-     -h|--help                  Displays this help
-     -v|--verbose               Displays verbose output
-    -nc|--no-colour             Disables colour output
-    -cr|--cron                  Run silently unless we encounter an error
-EOF
-}
+# Make `for f in *.txt` work when `*.txt` matches zero files
+shopt -s nullglob globstar
 
-# DESC: Parameter parser
-# ARGS: $@ (optional): Arguments provided to the script
-# OUTS: Variables indicating command-line parameters and options
-# RETS: None
-function parse_params() {
-    local param
-    while [[ $# -gt 0 ]]; do
-        param="$1"
-        shift
-        case $param in
-            -h | --help)
-                script_usage
-                exit 0
-                ;;
-            -v | --verbose)
-                verbose=true
-                ;;
-            -nc | --no-colour)
-                no_colour=true
-                ;;
-            -cr | --cron)
-                cron=true
-                ;;
-            *)
-                script_exit "Invalid parameter was provided: $param" 1
-                ;;
-        esac
-    done
-}
-
-# DESC: Main control flow
-# ARGS: $@ (optional): Arguments provided to the script
-# OUTS: None
-# RETS: None
-function main() {
-    trap script_trap_err ERR
-    trap script_trap_exit EXIT
-
-    script_init "$@"
-    parse_params "$@"
-    cron_init
-    colour_init
-    #lock_init system
-}
+# Set IFS to preferred implementation
+# IFS=$' '
 
 # shellcheck source=source.sh
-source "$(dirname "${BASH_SOURCE[0]}")/source.sh"
+source "/home/$USER/LocalRepository/bash-script-template/source.sh"
 
 # Invoke main with args if not sourced
 # Approach via: https://stackoverflow.com/a/28776166/8787985
